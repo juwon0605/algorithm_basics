88dcdfb3757014499e6a22a4acd5911b446dbf56
juwon0605 <58173061+juwon0605@users.noreply.github.com>
2021-01-19 16:42:49 +0900

    2. 자연수의 합
    자연수 A, B가 주어지면 A부터 B까지의 합을 수식과 함께 출력하세요.

---
+++ 1.코드구현력 기르기/2. 자연수의 합.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+using namespace std;
+
+/*
+2. 자연수의 합
+자연수 A, B가 주어지면 A부터 B까지의 합을 수식과 함께 출력하세요.
+*/
+
+int main() {
+	int a, b, sum = 0;
+	cin >> a >> b;
+	for (int i = a; i < b; i++) {
+		cout << i << " +";
+		sum += i;
+	}
+	cout << b << " = " << sum + b;
+	return 0;
+}
+
+/*
+모범 답안
+#include<stdio.h>
+int main(){
+	int a, b, sum = 0;
+	cin >> a >> b;
+	for (int i = a; i < b; i++) {
+		cout << i << " +";
+		sum += i;
+	}
+	cout << b << " = " << sum+b;
+	return 0;
+}
+*/
