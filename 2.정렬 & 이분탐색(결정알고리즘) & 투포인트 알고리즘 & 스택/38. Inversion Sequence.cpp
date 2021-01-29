ce0f2a2d76623d6344e8fb5561c4b6f8a6f92523
juwon0605 <58173061+juwon0605@users.noreply.github.com>
2021-01-29 18:56:22 +0900

    38. Inversion Sequence

--- 2.정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/38. Inversion Sequence.cpp
+++ 2.정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/38. Inversion Sequence.cpp
@@ -49,10 +49,29 @@ int main() {
 }
 /*
 모범 답안
-
-*/
-
-/*
-모범 답안 반영 전
-
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main() {
+	freopen("input.txt", "rt", stdin);
+	int n, i, j, pos;
+	scanf("%d", &n);
+	vector<int> is(n+1), os(n+1);
+	for(i=1; i<=n; i++){
+		scanf("%d", &is[i]);
+	}
+	for(i=n; i>=1; i--){
+		pos=i;
+		for(j=1; j<=is[i]; j++){
+			os[pos]=os[pos+1];
+			pos++;
+		}
+		os[pos]=i;
+	}
+	for(i=1; i<=n; i++){
+		printf("%d ", os[i]);
+	}
+	return 0;
+}
 */
