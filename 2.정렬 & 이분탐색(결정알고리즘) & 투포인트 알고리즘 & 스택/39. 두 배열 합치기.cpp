e9f6a6c3c8a03c99ba02e68e9e087e26db76e75e
juwon0605 <58173061+juwon0605@users.noreply.github.com>
2021-01-31 17:37:56 +0900

    40. 교집합(투포인터 알고리즘)

---
+++ 2.정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/39. 두 배열 합치기.cpp
@@ -0,0 +1,131 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+
+#include<iostream>
+using namespace std;
+
+/*
+39. 두 배열 합치기
+오름차순으로 정렬이 된 두 배열이 주어지면 두 배열을 오름차순으로 합쳐 출력하는 프로그램
+을 작성하세요.
+▣ 입력설명
+첫 번째 줄에 첫 번째 배열의 크기 N(1<=N<=100)이 주어집니다.
+두 번째 줄에 N개의 배열 원소가 오름차순으로 주어집니다.
+세 번째 줄에 두 번째 배열의 크기 M(1<=M<=100)이 주어집니다.
+네 번째 줄에 M개의 배열 원소가 오름차순으로 주어집니다.
+각 배열의 원소는 int형 변수의 크기를 넘지 않습니다.
+▣ 출력설명
+오름차순으로 정렬된 배열을 출력합니다.
+*/
+int main() {
+	//freopen("input.txt", "rt", stdin);
+	int a_cnt = 0, b_cnt = 0, res_cnt = 0;
+	int n;
+	cin >> n;
+	int *a = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	int m;
+	cin >> m;
+	int *b = new int[m];
+	for (int i = 0; i < m; i++) {
+		cin >> b[i];
+	}
+	int *res = new int[n + m];
+	while (a_cnt < n && b_cnt < m) {
+		if (a[a_cnt] >= b[b_cnt]) {
+			res[res_cnt++] = b[b_cnt++];
+		}
+		else {
+			res[res_cnt++] = a[a_cnt++];
+		}
+	}
+	while (a_cnt < n) res[res_cnt++] = a[a_cnt++];
+	while (b_cnt < m) res[res_cnt++] = b[b_cnt++];
+	for (int i = 0; i < res_cnt; i++) {
+		cout << res[i] << " ";
+	}
+	return 0;
+}
+/*
+모범 답안
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int a[101], b[101], c[300];
+int main(){
+	freopen("input.txt", "rt", stdin);
+	int n, m, i, p1=1, p2=1, p3=1;
+	scanf("%d", &n);
+	for(i=1; i<=n; i++){
+		scanf("%d", &a[i]);
+	}
+	scanf("%d", &m);
+	for(i=1; i<=m; i++){
+		scanf("%d", &b[i]);
+	}
+	while(p1<=n && p2<=m){
+		if(a[p1]<b[p2]){
+			c[p3++]=a[p1++];
+		}
+		else{
+			c[p3++]=b[p2++];
+		}
+	}
+	while(p1<=n) c[p3++]=a[p1++];
+	while(p2<=m) c[p3++]=b[p2++];
+	for(i=1; i<p3; i++){
+		printf("%d ", c[i]);
+	}
+	return 0;
+}
+*/
+
+/*
+모범 답안 반영 전
+int main() {
+	//freopen("input.txt", "rt", stdin);
+	int a_cnt = 0, b_cnt = 0, res_cnt = 0;
+	int n;
+	cin >> n;
+	int *a = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	int m;
+	cin >> m;
+	int *b = new int[m];
+	for (int i = 0; i < m; i++) {
+		cin >> b[i];
+	}
+	int *res = new int[n + m];
+	while (a_cnt < n && b_cnt < m) {
+		if (a[a_cnt] >= b[b_cnt]) {
+			res[res_cnt] = b[b_cnt];
+			b_cnt++;
+		}
+		else {
+			res[res_cnt] = a[a_cnt];
+			a_cnt++;
+		}
+		res_cnt++;
+	}
+	if (a_cnt == n) {
+		for (int i = b_cnt; i < m; i++) {
+			res[res_cnt] = b[i];
+			res_cnt++;
+		}
+	}
+	else {
+		for (int i = a_cnt; i < n; i++) {
+			res[res_cnt] = a[i];
+			res_cnt++;
+		}
+	}
+	for (int i = 0; i < n + m; i++) {
+		cout << res[i] << " ";
+	}
+	return 0;
+}
+*/
---
+++ 2.정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/40. 교집합(투포인터 알고리즘).cpp
@@ -0,0 +1,139 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+
+#include<iostream>
+using namespace std;
+
+/*
+40. 교집합(투포인터 알고리즘)
+두 집합 A, B가 주어지면 두 집합의 교집합을 출력하는 프로그램을 작성하세요.
+▣ 입력설명
+첫 번째 줄에 집합 A의 크기 N(1<=N<=30,000)이 주어집니다.
+두 번째 줄에 N개의 원소가 주어집니다. 원소가 중복되어 주어지지 않습니다.
+세 번째 줄에 집합 B의 크기 M(1<=M<=30,000)이 주어집니다.
+네 번째 줄에 M개의 원소가 주어집니다. 원소가 중복되어 주어지지 않습니다.
+각 집합의 원소는 int형 변수의 크기를 넘지 않습니다.
+▣ 출력설명
+두 집합의 교집합을 오름차순 정렬하여 출력합니다.
+*/
+int main() {
+	//freopen("input.txt", "rt", stdin);
+	int a_cnt = 0, b_cnt = 0, res_cnt = 0;
+	int m, n, min;
+	cin >> n;
+	int *a = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	cin >> m;
+	int *b = new int[m];
+	for (int i = 0; i < m; i++) {
+		cin >> b[i];
+	}
+	sort(a, a+n);
+	sort(b, b+m);
+	min = (n > m ? m : n);
+	int *res = new int[min];
+	while (a_cnt < n && b_cnt < m) {
+		if (a[a_cnt] < b[b_cnt]) a_cnt++;
+		else if (a[a_cnt] == b[b_cnt]) {
+			res[res_cnt++] = a[a_cnt];
+			a_cnt++;
+			b_cnt++;
+		}
+		else b_cnt++;
+	}
+	for (int i = 0; i < res_cnt; i++) {
+		cout << res[i] << " ";
+	}
+ 	return 0;
+}
+/*
+모범 답안
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+	freopen("input.txt", "rt", stdin);
+	int n, m, i, p1=0, p2=0, p3=0;
+	scanf("%d", &n);
+	vector<int> a(n);
+	for(i=0; i<n; i++){
+		scanf("%d", &a[i]);
+	}
+	sort(a.begin(), a.end());
+
+	scanf("%d", &m);
+	vector<int> b(m), c(m);
+	for(i=0; i<m; i++){
+		scanf("%d", &b[i]);
+	}
+	sort(b.begin(), b.end());
+
+	while(p1<n && p2<m){
+		if(a[p1]==b[p2]){
+			c[p3++]=a[p1++];
+			p2++;
+		}
+		else if(a[p1]<b[p2]){
+			p1++;
+		}
+		else p2++;
+	}
+	for(i=0; i<p3; i++){
+		printf("%d ", c[i]);
+	}
+	return 0;
+}
+*/
+
+/*
+모범 답안 반영 전
+void sort(int*, int);
+int main() {
+	//freopen("input.txt", "rt", stdin);
+	int a_cnt = 0, b_cnt = 0, res_cnt = 0;
+	int m, n, min;
+	cin >> n;
+	int *a = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	cin >> m;
+	int *b = new int[m];
+	for (int i = 0; i < m; i++) {
+		cin >> b[i];
+	}
+	sort(a, n);
+	sort(b, m);
+	min = (n > m ? m : n);
+	int *res = new int[min];
+	while (a_cnt < n && b_cnt < m) {
+		if (a[a_cnt] < b[b_cnt]) a_cnt++;
+		else if (a[a_cnt] == b[b_cnt]) {
+			res[res_cnt++] = a[a_cnt];
+			a_cnt++;
+			b_cnt++;
+		}
+		else b_cnt++;
+	}
+	for (int i = 0; i < res_cnt; i++) {
+		cout << res[i] << " ";
+	}
+	return 0;
+}
+void sort(int* arr, int size) {
+	for (int i = 0; i < size-1; i++) {
+		int min=arr[i], swap_idx=i;
+		for (int j = i + 1; j < size; j++) {
+			if (arr[j] < min) {
+				min = arr[j];
+				swap_idx = j;
+			}
+		}
+		int temp = arr[i];
+		arr[i] = arr[swap_idx];
+		arr[swap_idx] = temp;
+	}
+}
+*/
