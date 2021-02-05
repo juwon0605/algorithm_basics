c65efaca5e8d2e53f74a13627fc9567740a33571
juwon0605 <58173061+juwon0605@users.noreply.github.com>
2021-02-05 16:29:00 +0900

    51. 영지(territory) 선택 : (large)

---
+++ 2.정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/51. 영지(territory) 선택 (large).cpp
@@ -0,0 +1,135 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/*
+51. 영지(territory) 선택 : (large)
+세종대왕은 현수에게 현수가 다스릴 수 있는 영지를 하사하기로 했다. 전체 땅은 사각형으로 표
+시된다. 그 사각형의 땅 중에서 세종대왕이 현수가 다스릴 수 있는 땅의 크기(세로의 길이와 가
+로의 길이)를 정해주면 전체 땅 중에서 그 크기의 땅의 위치를 현수가 정하면 되는 것이다.
+전체 땅은 사각형의 모양의 격자로 되어 있으며, 그 사각형 땅 안에는 많은 오렌지 나무가 심
+겨져 있다. 현수는 오렌지를 무척 좋아하여 오렌지 나무가 가장 많이 포함되는 지역을 선택하
+고 싶어 한다. 현수가 얻을 수 있는 영지의 오렌지 나무 최대 개수를 출력하는 프로그램을 작
+성하세요. 다음과 같은 땅의 정보가 주어지고, 현수가 하사받을 크기가, 가로 2, 세로 3의 크
+기이면 가장 많은 오렌지 나무가 있는 영지는 총 오렌지 나무의 개수가 16인 3행 4열부터 시
+작하는 구역이다.
+3 5 1 3 1 3 2
+1 2 1 3 1 1 2
+1 3 1 5 1 3 4
+5 1 1 3 1 3 2
+3 1 1 3 1 1 2
+1 3 1 3 1 2 2
+▣ 입력설명
+첫 줄에 H(세로길이)와 W(가로길이)가 입력된다. (1<=H, W<=700) 그 다음 H줄에 걸쳐 각 사
+각형 지역에 오렌지의 나무 개수(1~9개) 정보가 주어진다.
+그 다음 영지의 크기인 세로길이(1~H)와 가로길이(1~W)가 차례로 입력된다.
+▣ 출력설명
+첫 줄에 현수가 얻을 수 있는 오렌지 나무의 최대 개수를 출력한다.
+*/
+int main() {
+	//freopen("input.txt", "rt", stdin);
+	int h, w, x, y, max = 0, sum;
+	cin >> h >> w;
+	int **terr = new int*[h + 1];
+	for (int i = 1; i <= w; i++) {
+		terr[i] = new int[w + 1];
+		for (int j = 1; j <= w; j++) {
+			cin >> terr[i][j];
+		}
+	}
+	cin >> y >> x;
+	vector<vector<int>> dp(h + 1, vector<int>(w + 1));
+	for (int i = 1; i <= h; i++) {
+		for (int j = 1; j <= w; j++) {
+			dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + terr[i][j];
+		}
+	}
+	for (int i = y; i <= h; i++) {
+		for (int j = x; j <= w; j++) {
+			sum = dp[i][j] - dp[i - y][j] - dp[i][j - x] + dp[i - y][j - x];
+			if (max < sum) max = sum;
+		}
+	}
+	cout << max;
+	return 0;
+}
+/*
+모범 답안
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int a[701][701], dy[701][701];
+int main(){
+	freopen("input.txt", "rt", stdin);
+	int h, w, n, m, i, j, tmp, max=-2147000000;
+	scanf("%d %d", &h, &w);
+	for(i=1; i<=h; i++){
+		for(j=1; j<=w; j++){
+			scanf("%d", &a[i][j]);
+			dy[i][j]=dy[i-1][j]+dy[i][j-1]-dy[i-1][j-1]+a[i][j];
+		}
+	}
+	scanf("%d %d", &n, &m);
+	for(i=n; i<=h; i++){
+		for(j=m; j<=w; j++){
+			tmp=dy[i][j]-dy[i-n][j]-dy[i][j-m]+dy[i-n][j-m];
+			if(tmp>max) max=tmp;
+		}
+	}
+	printf("%d\n", max);
+	return 0;
+}
+*/
+
+/*
+모범 답안 반영 전
+int main() {
+	//freopen("input.txt", "rt", stdin);
+	int h, w, x, y, sum, temp_sum=0, max = 0;
+	cin >> h >> w;
+	int **terr = new int*[h];
+	for (int i = 0; i < h; i++) {
+		terr[i] = new int[w];
+		for (int j = 0; j < w; j++) {
+			cin >> terr[i][j];
+		}
+	}
+	cin >> y >> x;
+	// 초기 1회 반복
+	for (int i = 0; i < y; i++) {
+		for (int j = 0; j < x; j++) {
+			temp_sum += terr[i][j];
+		}
+	}
+	sum = temp_sum;
+	if (max < sum) max = sum;
+	for (int j = x; j < w; j++) {
+		for (int k = 0; k < y; k++) {
+			sum -= terr[k][j - x];
+			sum += terr[k][j];
+		}
+		if (max < sum) max = sum;
+	}
+	// 이후 반복
+	for (int i = y; i < h; i++) {
+		for (int j = 0; j < x; j++) {
+			temp_sum -= terr[i - y][j];
+			temp_sum += terr[i][j];
+		}
+		sum = temp_sum;
+		if (max < sum) max = sum;
+		for (int j = x; j < w; j++) {
+			for (int k = y-1; k >= 0; k--) {
+				sum -= terr[i-k][j - x];
+				sum += terr[i-k][j];
+			}
+			if (max < sum) max = sum;
+		}
+	}
+	cout << max;
+	return 0;
+}
+*/
